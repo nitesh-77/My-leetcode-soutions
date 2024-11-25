221c867ebc86b67c7c8bf67b250cdda1f196c25e
nitesh <Nitesh33lol@gmail.com>
2024-11-25 20:25:25 +0530

    contains-duplicate

---
+++ javascript/contains-duplicate.js
@@ -0,0 +1,12 @@
+var containsDuplicate = function(nums) {
+    const numSet = new Set();
+
+    for (const n of nums) {
+        if (numSet.has(n)) {
+            return true;
+        }
+        numSet.add(n);
+    }
+    
+    return false;
+};
